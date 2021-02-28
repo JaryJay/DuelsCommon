bb64e7d29b9ec0a0f4489a7e59d1295da270696f
JaryJay <47123100+JaryJay@users.noreply.github.com>
2021-02-27 20:17:25 -0500

    Renamed MessageSentDataEvent to MessageSentGameEvent

--- src/main/java/event/MessageSentGameEvent.java
+++ src/main/java/event/MessageSentGameEvent.java
@@ -3,11 +3,12 @@ package event;
 import common.event.GameEvent;
 import common.source.GameSource;
 
-public class MessageSentDataEvent extends GameEvent {
+public class MessageSentGameEvent extends GameEvent {
 
+	private static final long serialVersionUID = -30334008891265989L;
 	private String message;
 
-	public MessageSentDataEvent(String message, long time, GameSource source) {
+	public MessageSentGameEvent(String message, long time, GameSource source) {
 		super(time, source);
 		this.message = message;
 	}
