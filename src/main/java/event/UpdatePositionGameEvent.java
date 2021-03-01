433fc481ff2cc05de5b2e85e17aeb56dde223ab6
JaryJay <47123100+JaryJay@users.noreply.github.com>
2021-02-28 20:34:21 -0500

    Added serialVersionUID to UpdatePositionGameEvent

--- src/main/java/event/UpdatePositionGameEvent.java
+++ src/main/java/event/UpdatePositionGameEvent.java
@@ -5,6 +5,8 @@ import common.source.GameSource;
 
 public class UpdatePositionGameEvent extends GameEvent {
 
+	private static final long serialVersionUID = -9150907298732338701L;
+
 	public UpdatePositionGameEvent(long time, GameSource source) {
 		super(time, source);
 	}
